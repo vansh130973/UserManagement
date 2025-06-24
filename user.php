2f7fb876a287d951dbedd460454cddb4916f02f5
Rudra616 <rudrampanchal@gmail.com>
2025-06-24 20:46:58 +0530

    User login successfully then redirect user.php

---
+++ user.php
@@ -0,0 +1,15 @@
+<?php require_once 'config.php';
+include 'include/header.php';
+?>
+  <div class="d-grid gap-2 d-md-flex justify-content-md-end float-right float-top">
+    <button class="btn btn-danger me-md-2 btn-hover-bg-shade-amount:15%;" type="button"><a
+        style="color: white; text-decoration: none;" href="logout.php">Logout</a></button>
+  </div>
+<div class="alert alert-success" role="alert">
+  Successfully Login!!!
+</div>
+</body>
+<?php include 'include/footer.php'; ?>
+<!-- class="d-grid gap-2 d-md-flex justify-content-md-end" -->
+
+</html>
